cae405879a74b2ed413064fa04715ae3769cf405
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-08 02:17:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+
+## Description of the directory structure 🎯
+
+- `assets/:` Contains static resources such as fonts and images.
+
+- `components/:` It is organized in logical subfolders (common, wallet, transactions) to separate reusable components by functionality.
+
+- `config/:` Application configurations such as paths and themes.
+
+- `hooks/:` Custom hooks are placed here.
+
+- `layouts/:` Reusable layouts that structure the application pages.
+
+- `lib/:` Utility functions and project's own libraries.
+
+- `pages/:` Contains all the pages that represent complete views.
+
+- `services/:` For logic that interacts with APIs or other external services.
+
+- `styles/:` Global and application-specific styles.
+
+- `App.tsx` and main.tsx: Maintain their main roles as the root component and the entry point of the application, respectively.
---
+++ tailwind.config.js
@@ -0,0 +1,105 @@
+/** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1C7C54",
+        secondary: "#73E2A7",
+        background: "#DEF4C6",
+        text: "#1B512D",
+        accent: "#B1CF5F",
+        black: "#000000", // Negro
+        darkGray: "#2F2F2F", // Gris oscuro
+      },
+      transitionProperty: {
+        shadow: "box-shadow",
+        colors: "background-color, border-color, color, fill, stroke",
+      },
+      transitionDuration: {
+        300: "300ms",
+        500: "500ms",
+      },
+      fontFamily: {
+        sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        hide: {
+          from: { opacity: "1" },
+          to: { opacity: "0" },
+        },
+        slideDownAndFade: {
+          from: { opacity: "0", transform: "translateY(-6px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        slideLeftAndFade: {
+          from: { opacity: "0", transform: "translateX(6px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+        slideUpAndFade: {
+          from: { opacity: "0", transform: "translateY(6px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        slideRightAndFade: {
+          from: { opacity: "0", transform: "translateX(-6px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+        accordionOpen: {
+          from: { height: "0px" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        accordionClose: {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: { height: "0px" },
+        },
+        dialogOverlayShow: {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+        dialogContentShow: {
+          from: {
+            opacity: "0",
+            transform: "translate(-50%, -45%) scale(0.95)",
+          },
+          to: { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+        },
+        drawerSlideLeftAndFade: {
+          from: { opacity: "0", transform: "translateX(100%)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+        drawerSlideRightAndFade: {
+          from: { opacity: "1", transform: "translateX(0)" },
+          to: { opacity: "0", transform: "translateX(100%)" },
+        },
+      },
+      animation: {
+        hide: "hide 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideDownAndFade:
+          "slideDownAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideLeftAndFade:
+          "slideLeftAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideUpAndFade: "slideUpAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideRightAndFade:
+          "slideRightAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        // Accordion
+        accordionOpen: "accordionOpen 150ms cubic-bezier(0.87, 0, 0.13, 1)",
+        accordionClose: "accordionClose 150ms cubic-bezier(0.87, 0, 0.13, 1)",
+        // Dialog
+        dialogOverlayShow:
+          "dialogOverlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        dialogContentShow:
+          "dialogContentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        // Drawer
+        drawerSlideLeftAndFade:
+          "drawerSlideLeftAndFade 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        drawerSlideRightAndFade: "drawerSlideRightAndFade 150ms ease-in",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
