ef366611a2594c4a826b19422a0cf56d3fd89ad1
Samuel Sml <veramirandasamuel6@gmail.com>
2024-09-08 02:26:07 -0500

    feat: develop HeroSection component

---
+++ src/components/common/HeroSection.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+const HeroSection = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: any) => {
+    setMousePosition({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const x = mousePosition.x / window.innerWidth - 0.5;
+  const y = mousePosition.y / window.innerHeight - 0.5;
+
+  return (
+    <section id="home" className="bg-gradient-to-b from-black to-primary min-h-screen flex items-center justify-center relative overflow-hidden px-6 md:px-12">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between relative z-10">
+        <div className="w-full md:w-1/2 text-center md:text-left mb-8 md:mb-0 px-4">
+          <motion.h1
+            className="text-4xl md:text-6xl font-bold text-accent mb-6"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+          >
+            Transforming Agriculture with Blockchain
+          </motion.h1>
+          <motion.p
+            className="text-lg md:text-xl text-white mb-6"
+            initial={{ opacity: 0, y: -30 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 30 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+          >
+            Innovating crop management through advanced tokenization and
+            blockchain technology.
+          </motion.p>
+          <button className="bg-accent text-text py-3 px-6 md:px-8 rounded-lg hover:bg-primary hover:text-white transition-colors duration-300">
+            Learn More
+          </button>
+        </div>
+
+        <div
+          className="w-full md:w-1/2 flex justify-center md:justify-end"
+          style={{
+            transform: `translate3d(${x * 15}px, ${y * 15}px, 0)`,
+            transition: "transform 0.5s ease-out",
+          }}
+        >
+          <motion.img
+            src="/src/assets/images/uchuvalabs-logo-transp.png"
+            alt="3D Image"
+            className="w-full max-w-xl h-auto"
+            animate={{ y: [-10, 10, -10] }}
+            transition={{
+              duration: 4,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
